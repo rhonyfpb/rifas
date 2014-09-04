2177fb80fe7016b89e3b0deca2948d07fb939c9c
Rhony Pedraza <rhony25@gmail.com>
2014-09-03 23:30:00 -0500

    Configuración de puertos y entorno
    Se configuran puertos y entornos, además de algunos templates.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var express = require("express");
+var routes = require("./routes.js");
 var handlebars = require("express-handlebars").create({
 	defaultLayout: "main"
 });
@@ -9,12 +10,31 @@ var app = express();
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
+app.set("port", process.env.PORT || 3000);
+
+// login
+switch(app.get("env")) {
+	case "development":
+		app.use(require("morgan")("dev"));
+		break;
+}
+
+// routes
+routes(app);
+
 // 404 handler
 app.use(function(request, response, next) {
 	response.status(404);
 	response.render("404");
 });
 
-http.createServer(app).listen(3000, function() {
-	console.log("Application started on http://localhost:3000; press Ctrl-C to terminate.");
+// 500 handler
+app.use(function(error, request, response, next) {
+	console.error(error.stack);
+	response.status(500);
+	response.render("500");
+});
+
+http.createServer(app).listen(app.get("port"), function() {
+	console.log("Application started in " + app.get("env") + " mode on http://localhost:" + app.get("port") + "; press Ctrl-C to terminate.");
 });
